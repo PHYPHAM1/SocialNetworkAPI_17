416c4f81fa08736cdb3a55c7e361c7cfcbb1bbf7
Phy <pham.phy@gmail.com>
2024-10-07 21:49:16 -0500

    model completed

---
+++ Main/src/models/user.ts
@@ -0,0 +1,54 @@
+import { Schema, model, Document, ObjectId } from 'mongoose';
+
+interface IUser extends Document {
+    username?: string;
+    email?: string;
+    thoughts: ObjectId[];
+    friends: ObjectId[];   //Array of _id values referencing the User model (self-reference)???
+}
+
+const userSchema = new Schema<IUser>(
+    {  //object 1
+    username: { 
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+    },
+    thoughts: [{ 
+        type: Schema.Types.ObjectId, 
+            ref: 'Thought' 
+        },
+        ],
+
+    friends: [
+        { type: Schema.Types.ObjectId, 
+            ref: 'User' 
+        },
+        ],
+    },
+    {   //object 2
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+
+//create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function(this: any) {
+    return this.friends;
+})
+.set(function(this: any) {
+    return this.friends.length;
+});
+
+//initialize the User model
+const User = model('User', userSchema);
+
+export default User;

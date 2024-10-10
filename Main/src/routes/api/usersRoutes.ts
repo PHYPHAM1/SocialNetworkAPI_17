f258de5a45ecfa2817adb1967230c94457ccd800
Phy <pham.phy@gmail.com>
2024-10-10 13:25:36 -0500

    tested, able to build and run the seed data

--- Main/src/routes/api/usersRoutes.ts
+++ Main/src/routes/api/usersRoutes.ts
@@ -3,7 +3,8 @@ const router = Router();
 import { createUsers , getUsers , getSingleUser } from '../../controllers/userController';
 
 //api users, get all users, and  create a user (post)
-router.route('/api/users').get(getUsers).post((createUsers));
+router.route('/api/users').get(getUsers).post((createUsers));  
+//TODOS: DO I NEED THIS?? IF I ALREADY HAVE  app.post, app.get routes for users in server.ts
 
 //api/users/:id, get a single user
 router.route('/api/user/:id').get(getSingleUser);
